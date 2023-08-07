6c26c3868225b58d5c59b38188241beb6698691f
Damian Borowiecki <dborowiecki@soldevelo.com>
2023-08-07 17:34:51 +0200

    Adjusted for tox tests execution

--- modules-requirements.py
+++ modules-requirements.py
@@ -1,15 +1,21 @@
 import os
 import json
 import sys
+import argparse
 
+parser = argparse.ArgumentParser(description='Generate requirements from openIMIS json config.')
+parser.add_argument('filename')    
+parser.add_argument('-s', '--save', action='store_true', help='Save output to a file')
+
+args = parser.parse_args()
 
 sys.path.insert(0, './openIMIS/openIMIS')
 from openimisconf import load_openimis_conf
 
 conf_file_path = 'openimis.json'
 
-if len(sys.argv) > 1 :
-    conf_file_path = sys.argv[1]
+if parser.filename :
+    conf_file_path = parser.filename
 
 if not conf_file_path:
     sys.exit("Missing config file path argument")
@@ -22,4 +28,10 @@ def extract_requirement(module):
 
 OPENIMIS_CONF = load_openimis_conf(conf_file_path)
 MODULES = list(map(extract_requirement, OPENIMIS_CONF["modules"]))
-print("\n".join(MODULES))
+
+
+if args.save:
+    with open('modules-requirements-generated.txt', 'w') as f:
+        f.write(output_data)
+else:
+    print(output_data)
